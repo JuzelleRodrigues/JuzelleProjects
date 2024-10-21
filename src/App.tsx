fcad0bbcc023dc97eb6a770c82c3a5eaedf2ddc2
JuzelleRodrigues <juzellerodigues1006@gmail.com>
2024-10-21 17:56:44 +0100

    src/

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './assets/Pages/NavBar';
+import About from './assets/Pages/About';
+import Contact from './assets/Pages/Contact';
+import Projects from './assets/Pages/Projects';
+import VideoGallery from './assets/Pages/VideoGallery';
+import WorkExperience from './assets/Pages/WorkExperience';
+import Blog from './assets/Pages/Blog';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/videogallery" element={<VideoGallery />} />
+          <Route path="/workexperience" element={<WorkExperience />} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/assets/Pages/Blog.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Blog.css';
+
+const Blog: React.FC = () => {
+  return (
+    <div className="blog-container">
+      {/* <h2>About Me</h2> */}
+      <p>Welcome to the About page. Here you can write about yourself or your project.</p>
+    </div>
+  );
+};
+
+export default Blog;
